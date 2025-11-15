7891f6674c68ed4e59a491c4f9979bc812f6d5c4
jhkoo0314 <jhyun06270314@gmail.com>
2025-11-15 20:51:07 +0900

    절대 경로로 타입 재수출 변경: Vercel 빌드 환경 호환성 개선

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -3,17 +3,18 @@
 // Version: 2.0
 // Last Updated: 2025-11-07
 
-export * from "./property";
-export * from "./valuation";
-export * from "./rights";
-export * from "./cost";
-export * from "./profit";
-export * from "./courtdocs";
-export * from "./result";
+// Vercel 빌드 환경 호환성을 위해 절대 경로 사용
+export * from "@/lib/types/property";
+export * from "@/lib/types/valuation";
+export * from "@/lib/types/rights";
+export * from "@/lib/types/cost";
+export * from "@/lib/types/profit";
+export * from "@/lib/types/courtdocs";
+export * from "@/lib/types/result";
 
 // Explicit re-exports for commonly used types (to ensure TypeScript resolution)
-export { DifficultyMode, PropertyCategory, PropertyType } from "./property";
-export type { PropertySeed, Property } from "./property";
+export { DifficultyMode, PropertyCategory, PropertyType } from "@/lib/types/property";
+export type { PropertySeed, Property } from "@/lib/types/property";
 
 // Policy types (SSOT re-export)
 export type { Policy, PolicyOverrides } from "@/lib/policy/policy";
